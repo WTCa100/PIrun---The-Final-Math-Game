7b87005e310e059a8fd4aeb2187e7434c8bb3ab4
John Peter Bielawa <janbielawa00@gmail.com>
2022-09-11 20:03:29 +0200

    Added comments

--- PIrun/Utilities/Menu/Menu.cpp
+++ PIrun/Utilities/Menu/Menu.cpp
@@ -1,9 +1,23 @@
 #pragma once
 #include"Menu.h"
 
+void Menu::mainDisplay()
+{
+	std::cout << "Welcome to PIrun - the final math game!\n";
+	std::cout << "Options:\n";
+}
+
+Menu::Menu()
+{
+	InitialBootUp = false;
+	checkEssentialDirectories();
+}
+
+/*The following methods are used to validate wether or not essential directories exists*/
 
 struct stat info;
 
+// Creates directory
 void Menu::makeDir(int fType)
 {
 	std::string _exBatFile = MAKE_FILES;
@@ -22,7 +36,7 @@ void Menu::makeDir(int fType)
 	}
 	std::cout << "Creating file...\n";
 	std::cout << "Executing " << _exBatFile << std::endl;
-	_exBatFile += " >nul";
+	_exBatFile += " >nul"; // This is used to not display logs from cmdlet
 	system(_exBatFile.c_str());
 }
 
@@ -33,6 +47,7 @@ void Menu::checkEssentialDirectories()
 	checkDir(PROBLEM_SET);
 }
 
+// Functions checks and makes files found missing
 void Menu::checkDir(std::string _FILENAME)
 {
 	bool bDoesDirExists = false;
@@ -47,32 +62,19 @@ void Menu::checkDir(std::string _FILENAME)
 			else if (_FILENAME == DETAILED_RECORDS)
 				makeDir(2);
 			else if (_FILENAME == PROBLEM_SET)
-				makeDir	(3);
+				makeDir(3);
 			else { std::cout << "Unknown error orccured.\n"; system("exit"); }
 		}
 	} while (!bDoesDirExists);
 }
-
+// Function checks if specific Dir exists using stat info
 bool Menu::lookForDir(std::string _DIRpath)
 {
-	if (stat(_DIRpath.c_str(), &info) != 0)
+	if (stat(_DIRpath.c_str(), &info) != 0) // If dir was found succesfully return 0 if not returns -1
 	{
 		std::cout << "Cannot access " << _DIRpath << "\n";
 		return false;
 	}
 	std::cout << _DIRpath << " found!\n";
-	std::string Command = "cd " + _DIRpath;
 	return true;
-}
-
-void Menu::mainDisplay()
-{
-	std::cout << "Welcome to PIrun - the final math game!\n";
-	std::cout << "Options:\n";
-}
-
-Menu::Menu()
-{
-	InitialBootUp = false;
-	checkEssentialDirectories();
 }
