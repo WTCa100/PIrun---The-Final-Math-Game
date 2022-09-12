979a5cecaa9dbda6ef01b86d147697407e0ab08d
John Peter Bielawa <janbielawa00@gmail.com>
2022-09-12 10:47:12 +0200

    Added filenames to `paths.h`

--- PIrun/Utilities/Menu/Pathing/Paths.h
+++ PIrun/Utilities/Menu/Pathing/Paths.h
@@ -1,6 +1,9 @@
 #pragma once
-#define HIGHSCORES "Gamestate/"
+#define SCORES "Gamestate/"
 #define DETAILED_RECORDS "Gamestate/Details"
 #define PROBLEM_SET "Gamestate/Details/Problems"
 
 #define MAKE_FILES "Utilities\\Fmake\\"
+
+#define HIGHSCORES_TXT "Highscores.txt"
+#define SCOREBOARD_CSV "Scoreboard.csv"
