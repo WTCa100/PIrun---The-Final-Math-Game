c0358b3de9378d772812dc7149bf6e592629304f
John Peter Bielawa <janbielawa00@gmail.com>
2022-09-14 00:38:38 +0200

    Added the initial player class constructor with value initalization

--- PIrun/Utilities/Player.cpp
+++ PIrun/Utilities/Player.cpp
@@ -4,7 +4,7 @@
 
 int Player::getLatestId()
 {
-	if (Menu::InitialBootUp)
+	if (GameState::InitialBootUp)
 		nID = 1;
 	else
 	{
@@ -14,6 +14,19 @@ int Player::getLatestId()
 	return 0;
 }
 
+void Player::getInitialUserName()
+{
+	do
+	{
+		std::cout << "Enter your username:\n";
+		std::getline(std::cin, this->strUsername);
+		if (Validate::isEntryEmpty(this->strUsername))
+			std::cout << "Nickname must be at least 1 character long!\n";
+
+	} while (Validate::isEntryEmpty(this->strUsername));
+}
+
 Player::Player()
 {
+	getInitialUserName();
 }
--- PIrun/Utilities/Player.h
+++ PIrun/Utilities/Player.h
@@ -1,16 +1,19 @@
 #pragma once
 #include<string>
 #include<fstream>
+#include <iostream>
 
-#include "Menu/Menu.h"
+#include "Menu/GameState/Gamestate.h"
+#include "Menu/InputValidator/InputCheck.h"
 #include "Menu/Pathing/Paths.h"
 
-class Player : protected Menu
+class Player
 {
 private:
 	int nID;
 	std::string strUsername;
-	int getLatestId();
+	int getLatestId(); // Obtain the ID value to give to the latest player
+	void getInitialUserName(); // Constructor - get player nickname info
 public:
 	Player();
 
