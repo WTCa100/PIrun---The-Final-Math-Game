0f7c95aef1417c1403d0eea518915f820616c10e
John Peter Bielawa <janbielawa00@gmail.com>
2022-09-14 20:52:46 +0200

    Included Ctime and added rand seed

--- PIrun/Main.cpp
+++ PIrun/Main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <ctime>
 #include"Utilities/Menu/Menu.h"
 
 int main()
 {
+	srand((unsigned)time(NULL));
 	Menu* gameMenu = new Menu;
 	gameMenu->mainDisplay();
 	delete gameMenu;
