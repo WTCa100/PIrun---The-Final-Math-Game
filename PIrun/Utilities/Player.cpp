22ca348d14558df7c707f04d10a4465c35573a11
John Peter Bielawa <janbielawa00@gmail.com>
2022-09-16 21:31:36 +0200

    `Player` class now can `SavePlayerProgress` and `DisplayProblems`

--- PIrun/Utilities/Player.cpp
+++ PIrun/Utilities/Player.cpp
@@ -27,13 +27,44 @@ void Player::getInitialUserName()
 	} while (Validate::isEntryEmpty(this->strUsername));
 }
 
+
+
+void Player::SavePlayerProgress(std::ofstream &filePtr)
+{
+	for (auto i = ProblemSet.begin(); i != ProblemSet.end(); i++)
+	{
+		i->second.SaveProblem(filePtr);
+	}
+}
+
+int Player::givePlayerId()
+{
+	return this->nID;
+}
+
+void Player::DisplayProblems()
+{
+	for (auto i = ProblemSet.begin(); i != ProblemSet.end(); i++)
+	{
+		std::cout << i->first << ": ";
+		i->second.showProblemDone();
+	}
+}
+
+
+void Player::AssignProblem(Problem currentProblem)
+{
+	this->ProblemSet.insert(std::make_pair(currentProblem.nProblemId, currentProblem));
+}
+
 void Player::AddPoint(int dbPointValue)
 {
-	this->dbPointsEarned + dbPointValue;
+	this->dbPointsEarned += dbPointValue;
 }
 
 Player::Player(bool isInitial)
 {
+	this->ProblemSet.clear();
 	this->isInitialLaunch = isInitial;
 	getLatestId();
 	getInitialUserName();
--- PIrun/Utilities/Player.h
+++ PIrun/Utilities/Player.h
@@ -2,13 +2,17 @@
 #include<string>
 #include<fstream>
 #include <iostream>
+#include <map>
+#include <fstream>
 
 #include "Menu/InputValidator/InputCheck.h"
 #include "Menu/Pathing/Paths.h"
+#include "ProblemGen.h"
 
 class Player
 {
 private:
+	std::map <int, Problem> ProblemSet;
 	double dbPointsEarned;
 	bool isInitialLaunch;
 	int nID;
@@ -16,7 +20,11 @@ private:
 	int getLatestId(); // Obtain the ID value to give to the latest player
 	void getInitialUserName(); // Constructor - get player nickname info
 public:
+	void SavePlayerProgress(std::ofstream &filePtr);
+	void SavePlayerToScoreboard(std::ofstream& filePtr); // Todo save player to scoreboard.csv
+	int givePlayerId();
+	void DisplayProblems(); // Debug
+	void AssignProblem(Problem currentProblem);
 	void AddPoint(int dbPointValue);
 	Player(bool isInitial);
-
 };
