de88209d6b6c0348b344c64c1d9179689f4d98dd
John Peter Bielawa <janbielawa00@gmail.com>
2022-09-17 17:43:58 +0200

    Fixed record string

--- PIrun/Utilities/Menu/Pathing/Paths.h
+++ PIrun/Utilities/Menu/Pathing/Paths.h
@@ -1,7 +1,7 @@
 #pragma once
 #define SCORES "GameFiles/"
-#define DETAILED_RECORDS "GameFiles/Details"
-#define PROBLEM_SET "GameFiles/Details/Problems"
+#define DETAILED_RECORDS "GameFiles/Details/"
+#define PROBLEM_SET "GameFiles/Details/Problems/"
 
 #define MAKE_FILES "Utilities\\Fmake\\"
 
