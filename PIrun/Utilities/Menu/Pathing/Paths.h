e5d47f36576dbddb955887599c9dd24349e92ced
John Peter Bielawa <janbielawa00@gmail.com>
2022-11-04 00:55:39 +0100

    Git commit bug

--- PIrun/Utilities/Menu/Pathing/Paths.h
+++ PIrun/Utilities/Menu/Pathing/Paths.h
@@ -3,7 +3,7 @@
 #define SCORES "GameFiles/"
 // This directory is generally a lefover after previous saving system and has not since been deleted
 // The reason why is that in the future I might get this in use somehow
-#define DETAILED_RECORDS "GameFiles/Details/"
+#define DETAILED_RECORDS "GameFiles/Details/" 
 #define PROBLEM_SET "GameFiles/Details/Problems/"
 // .bat scripts dir
 #define MAKE_FILES "Utilities\\Fmake\\"
