6aa6d09f8d0e053d0a2737b07a404a885959ae55
John Peter Bielawa <janbielawa00@gmail.com>
2022-11-04 00:55:22 +0100

    Deleted unnecessery game file macros

--- PIrun/Utilities/Menu/Pathing/Paths.h
+++ PIrun/Utilities/Menu/Pathing/Paths.h
@@ -1,11 +1,12 @@
 #pragma once
 // .bat scripts
 #define SCORES "GameFiles/"
+// This directory is generally a lefover after previous saving system and has not since been deleted
+// The reason why is that in the future I might get this in use somehow
 #define DETAILED_RECORDS "GameFiles/Details/"
 #define PROBLEM_SET "GameFiles/Details/Problems/"
 // .bat scripts dir
 #define MAKE_FILES "Utilities\\Fmake\\"
 // Gamefiles
 #define HIGHSCORES_CSV "Highscores.csv"
-#define SCOREBOARD_CSV "Scoreboard.csv"
-#define DETAILS_CSV "Details.csv"
+#define PLAYERS_DATA_CSV "PlayersData.csv"
